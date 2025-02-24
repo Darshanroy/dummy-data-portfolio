4586eb6bb99bdc93878089ac64c663cc315ecfc4
Darshan Kumar <141019519+Darshanroy@users.noreply.github.com>
2025-02-24 21:15:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/your-repo-name/", // Replace with your actual repository name
+  base: "/dummy-data-portfolio/", // Replace with your actual repository name
   server: {
     host: "::",
     port: 8080,
